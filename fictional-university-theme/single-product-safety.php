9e7e5fd0218f42513c17b3dbfe2f8db045baf8df
rachelHartfelder <113880300+rachelHartfelder@users.noreply.github.com>
2024-05-30 13:11:50 -0700

    Update single-product-safety.php

--- fictional-university-theme/single-product-safety.php
+++ fictional-university-theme/single-product-safety.php
@@ -1,8 +1,28 @@
 <?php get_header(); ?>
 
+<div class = "how-it-works">
+  <div class = "row">
+    <div class = "column-1">
+      <h2 class = "column-header">Product Testing</h2>
+      <p class = "step-description">Utilizing a physical testing apparatus and a computational analysis model, the wheelchair securement system was tested in a simulated flight environment.</p>
+    </div>
+    <h2 class = "column-header"></h2>
+    
+  </div>
 
-<div class="placeholder">
-    This is the "Safety" page
+  <div class = "row">
+    <div class = "column-1">
+    
+      <h2 class = "column-header">Validation Testing</h2>
+      <p class = "step-description">The wheelchair securement system followed testing requirements outlined by the FAA for plane seats.</p>
+      <img class = "step-image" src = "<?php echo get_theme_file_uri('/images/validation.png') ?>"/>
+    </div>
+    <div class = "column-2">
+    <h2 class = "column-header">Verification Testing</h2>
+      <p class = "step-description">User testing found that the method of securement using the wheelchair securment system was intuitive, realtively quick, and easy.</p>
+      <img class = "step-image" src = "<?php echo get_theme_file_uri('/images/verification.png') ?>"/>
+    </div>
+  </div>
 </div>
 
 

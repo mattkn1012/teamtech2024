d0a7359c2d0e8397127a40fbfd43ed33c006e345
rachelHartfelder <113880300+rachelHartfelder@users.noreply.github.com>
2024-05-30 13:11:21 -0700

    Update single-product-how-it-works.php

--- fictional-university-theme/single-product-how-it-works.php
+++ fictional-university-theme/single-product-how-it-works.php
@@ -1,7 +1,47 @@
 <?php get_header(); ?>
 
-<div class="placeholder">
-    This is the "How it Works" page
+<div class = "how-it-works">
+  <div class = "row">
+    <div class = "column-1">
+      <h2 class = "column-header">Step 1</h2>
+      <p class = "step-description">The wheelchair user will back up until they are lined up with the line marked as so. Adjust the U bars to be in line with the wheels by lifting the latch on the back and shifting it left/right. Then lower the latch to lock them into place.</p>
+    </div>
+    <div class = "column-2">
+      <img class = "step-image" src = "<?php echo get_theme_file_uri('/images/Step 1 .png') ?>"/>
+    </div>
+  </div>
+
+  <div class = "row">
+    <div class = "column-1">
+      <h2 class = "column-header">Step 2</h2>
+      <p class = "step-description">Adjust the width of the u-bars to fit around the wheels by positioning the u-bars on the track.
+    </div>
+    <div class = "column-2">
+    <img class = "step-image" src = "<?php echo get_theme_file_uri('/images/Step 3.png') ?>"/>
+    </div>
+  </div>
+  <div class = "row">
+    <div class = "column-1">
+      <h2 class = "column-header">Step 3</h2>
+      <p class = "step-description"> Lower the U-bars into place and lock them into place.
+</p>
+    </div>
+    <div class = "column-2">
+    <img class = "step-image" src = "<?php echo get_theme_file_uri('/images/Step 4.png') ?>"/>
+    </div>
+  </div>
+
+<div class = "row">
+    <div class = "column-1">
+      <h2 class = "column-header">Step 4</h2>
+      <p class = "step-description"> Next attach the front hooks to the front of the wheelchair chassis, underneath the user’s feet. Tighten the front straps down until they are secure.
+</p>
+    </div>
+    <div class = "column-2">
+    <img class = "step-image" src = "<?php echo get_theme_file_uri('/images/Step 5.png') ?>"/>
+    </div>
+  </div>
 </div>
+    
 
 <?php get_footer(); ?>
